02c22cdc1ce3f531a91ec74529217e3c91a50087
slyunian <1181717028@qq.com>
2019-04-02 15:04:01 +0800

    '这是第一次递交'

---
+++ src/components/home/home.vue
@@ -0,0 +1,103 @@
+<template>
+  <el-container class="home">
+    <el-header class="home-header">
+      <el-row>
+        <el-col :span="8">
+          <!-- logo -->
+          <div class="head-left">
+            <img src="../../comment/img/logo.png">
+          </div>
+        </el-col>
+        <el-col :span="8" align="middle" justify="center">
+          <div class="head-center">
+            <h3>黑马会员管理系统</h3>
+          </div>
+        </el-col>
+        <el-col :span="8">
+          <div class="head-right">
+            富婆年年有,今年特别多
+            <a href="javascript:;" @click="isoutlogin()">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="home-aside">Aside</el-aside>
+      <el-main class="home-main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  // beforeCreate () {
+  //   if (!localStorage.getItem('userinfo')) {
+  //     this.$message({
+  //       type: 'warning',
+  //       message: '请先登录'
+  //     })
+  //     this.$router.push('/login')
+  //   }
+  // },
+  methods: {
+    isoutlogin () {
+      this.$confirm('是否退出登录', '确认信息', {
+        distinguishCancelAndClose: true,
+        confirmButtonText: '退出',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '退出成功'
+          })
+          this.$router.push('/login')
+          localStorage.removeItem('userinfo')
+        })
+        .catch(action => {
+          this.$message({
+            type: 'info',
+            message: action === 'cancel' ? '已取消退出' : '已经取消退出'
+          })
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  height: 100%;
+}
+
+/* 头部 */
+.home-header {
+  height: 100%;
+  min-width: 900px;
+  background-color: #b3c1cd;
+}
+.head-center {
+  color: #ffffff;
+  font-size: 28px;
+  line-height: 60px;
+}
+.head-right {
+  line-height: 60px;
+  float: right;
+  padding-right: 30px;
+}
+.head-right a {
+  color: #daa520;
+}
+
+/* 侧边 */
+.home-aside {
+  height: 100%;
+  background-color: #545c64;
+}
+.home-main {
+  height: 100%;
+  background-color: #eaeef1;
+}
+</style>
---
+++ src/components/login/login.vue
@@ -0,0 +1,88 @@
+<template>
+  <el-row type="flex" justify="center" align="middle" class="row1">
+    <el-col :span="8" class="col1">
+      <el-form class="form" :model="ruleForm" :rules="rules" ref="ruleForm">
+        <el-form-item label="用户名:" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码:" prop="password">
+          <el-input v-model="ruleForm.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="startLogin()">登录</el-button>
+          <el-button @click="resetForm()">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 4, max: 6, message: '长度在 4 到 6 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    startLogin () {
+      this.$refs.ruleForm.validate(valid => {
+        if (!valid) {
+          return
+        }
+        this.$axios
+          .post('http://localhost:8888/api/private/v1/login', this.ruleForm)
+          .then(res => {
+            console.log(res)
+            // 判断是否登录成功
+            if (res.data.meta.status === 200) {
+              this.$message({
+                type: 'success',
+                message: '登录成功',
+                duration: 800
+              })
+              this.$router.push('/home')
+              // 登录保持
+              localStorage.setItem('userinfo', JSON.stringify(this.ruleForm))
+            } else {
+              this.$message({
+                type: 'error',
+                message: '登录失败',
+                duration: 800
+              })
+            }
+          })
+      })
+    },
+    resetForm () {
+      this.$refs.ruleForm.resetFields()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.row1 {
+  height: 100%;
+  background: #2d434c;
+}
+
+.col1 {
+  background: #fff;
+  padding: 25px 50px 0 30px;
+  border-radius: 5px;
+}
+</style>
